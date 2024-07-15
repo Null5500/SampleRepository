9e30195ee4790d9101e58d894afc143c3595c11f
Null5500 <albertomolina5500@gmail.com>
2024-07-15 15:16:22 -0400

    message

--- SampleFolder/calculateArea/calculate_Area.js
+++ SampleFolder/calculateArea/calculate_Area.js
@@ -8,3 +8,18 @@ width = parseFloat(document.getElementById('width').value);
 let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let amount1;
+let amount2;
+let amount3;
+
+function groceryTracker(){
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let totalamount = amount1 + amount2 + amount3;
+    document.getElementById('result2').innerText = `The total amount spent on the grocery purchase is: $${totalamount}`;
+}
+    
+
